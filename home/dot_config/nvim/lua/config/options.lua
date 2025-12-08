7f7500439d47ad944ea39bd30cea123f93dfe402
Martin Emde <me@martinemde.com>
2025-12-08 14:45:25 -0800

    Use nvim absolute line numbers

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Use absolute line numbers instead of relative
+vim.opt.relativenumber = false
+
 -- Automatically reload files when changed outside of Neovim
 vim.opt.autoread = true
 
