4b1ac510e516e65414b94ef3bcaceb7b825fa32d
Martin Emde <me@martinemde.com>
2025-12-19 20:06:22 -0800

    Try disabling default tokyo theme in nvim
    It sometimes seems to revert to it

--- home/dot_config/nvim/lua/plugins/catppuccin.lua
+++ home/dot_config/nvim/lua/plugins/catppuccin.lua
@@ -1,91 +1,95 @@
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  priority = 1000,
-  name = "catppuccin",
-  opts = {
-    flavour = "auto", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-      light = "latte",
-      dark = "mocha",
-    },
-    transparent_background = true, -- disables setting the background color.
-    float = {
-      transparent = false, -- enable transparent floating windows
-      solid = false, -- use solid styling for floating windows, see |winborder|
-    },
-    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-    dim_inactive = {
-      enabled = false, -- dims the background color of inactive window
-      shade = "dark",
-      percentage = 0.15, -- percentage of the shade to apply to the inactive window
-    },
-    no_italic = false, -- Force no italic
-    no_bold = false, -- Force no bold
-    no_underline = false, -- Force no underline
-    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-      comments = { "italic" }, -- Change the style of comments
-      conditionals = { "italic" },
-      loops = {},
-      functions = {},
-      keywords = {},
-      strings = {},
-      variables = {},
-      numbers = {},
-      booleans = {},
-      properties = {},
-      types = {},
-      operators = {},
-      -- miscs = {}, -- Uncomment to turn off hard-coded styles
-    },
-    lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
-      virtual_text = {
-        errors = { "italic" },
-        hints = { "italic" },
-        warnings = { "italic" },
-        information = { "italic" },
-        ok = { "italic" },
+  { "tiagovla/tokyodark.nvim", enabled = false },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    name = "catppuccin",
+    opts = {
+      flavour = "auto", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "mocha",
       },
-      underlines = {
-        errors = { "underline" },
-        hints = { "underline" },
-        warnings = { "underline" },
-        information = { "underline" },
-        ok = { "underline" },
+      transparent_background = true, -- disables setting the background color.
+      float = {
+        transparent = false, -- enable transparent floating windows
+        solid = false, -- use solid styling for floating windows, see |winborder|
       },
-      inlay_hints = {
-        background = true,
+      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+      no_italic = false, -- Force no italic
+      no_bold = false, -- Force no bold
+      no_underline = false, -- Force no underline
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      },
+      lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+          ok = { "italic" },
+        },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+          ok = { "underline" },
+        },
+        inlay_hints = {
+          background = true,
+        },
+      },
+      color_overrides = {
+        mocha = {
+          base = "#0f0f19",
+          mantle = "#0a0a13",
+          crust = "#05050c",
+        },
+      },
+      custom_highlights = function(colors)
+        return {
+          WinSeparator = { fg = colors.blue },
+        }
+      end,
+      default_integrations = true,
+      auto_integrations = true,
+      integrations = {
+        blink_cmp = true,
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
       },
     },
-    color_overrides = {
-      mocha = {
-        base = "#0f0f19",
-        mantle = "#0a0a13",
-        crust = "#05050c",
-      },
-    },
-    custom_highlights = function(colors)
-      return {
-        WinSeparator = { fg = colors.blue },
-      }
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
     end,
-    default_integrations = true,
-    auto_integrations = true,
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      nvimtree = true,
-      notify = false,
-      mini = {
-        enabled = true,
-        indentscope_color = "",
-      },
-      -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    },
   },
-  config = function(_, opts)
-    require("catppuccin").setup(opts)
-    vim.cmd.colorscheme("catppuccin")
-  end,
 }
