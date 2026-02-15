d256a92ce9b851eff5e07daebdb16914c1bc5eac
Martin Emde <me@martinemde.com>
2026-02-15 10:11:25 -0800

    Add tmux-style keybindings to zellij with Ctrl+b prefix
    Replace the unlock-first (Ctrl+g) workflow with a tmux-like Ctrl+b
    prefix key. Normal mode is now the default with autolock handling
    vim/fzf detection. Ctrl+hjkl navigates panes in non-locked modes,
    and Ctrl+g is completely free for applications like Claude Code.
    
    Also replace vim-tmux-navigator with conditional loading of
    zellij-nav.nvim (in zellij) or vim-tmux-navigator (in tmux).

--- home/dot_config/nvim/lua/plugins/tmux-navigator.lua
+++ home/dot_config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,17 +1,35 @@
 return {
-  "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
+  -- Zellij navigation (active when inside zellij)
+  {
+    "swaits/zellij-nav.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    cond = function() return vim.fn.getenv("ZELLIJ") ~= vim.NIL end,
+    keys = {
+      { "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>",  { silent = true, desc = "navigate left or tab" } },
+      { "<c-j>", "<cmd>ZellijNavigateDown<cr>",     { silent = true, desc = "navigate down" } },
+      { "<c-k>", "<cmd>ZellijNavigateUp<cr>",       { silent = true, desc = "navigate up" } },
+      { "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
+    },
+    opts = {},
   },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  -- Tmux navigation (active when inside tmux, preserves existing workflow)
+  {
+    "christoomey/vim-tmux-navigator",
+    cond = function() return vim.fn.getenv("TMUX") ~= vim.NIL end,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
   },
 }
